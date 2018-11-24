68914f7388196134a1a08d6d363b96250e6055ef
Rezfix <44650563+Rezfix@users.noreply.github.com>
2018-11-24 07:07:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,46 +15,57 @@ client.on('ready', () => {
       console.log(`Logged in as * [ " ${client.user.username} " ] channels! [ " ${client.channels.size} " ]`);
   });
 
+const seender = 'لعمل منشن لمرسل الرساله قم بكتابة {sender} في الرسالة.';
+const server = 'لكتابة اسم السيرفر قم بكتابة {server} في الرسالة.';
+const user = 'لعمل منشن للشخص قم بكتابة {user} في الرسالة.';
 client.on('message', message => {
-	if(command == prefix + 'bc') {
-		if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('\`\`ADMINISTRATOR\`\` **انت لا تمتلك صلاحية**')
-		if(!args1) return message.channel.send(`**➥ Useage:** ${prefix}bc (كلامك)`);
-
-		let bcSure = new Discord.RichEmbed()
-		.setTitle(`:mailbox_with_mail: **هل انت متأكد انك تريد ارسال هذه الرسالة الى** ${message.guild.memberCount} **عضو؟**`)
-		.setThumbnail(client.user.avatarURL)
-		.setColor('GRAY')
-		.setDescription(`**\n:envelope: ➥ الرسالة**\n\n${args1}`)
-		.setTimestamp()
-		.setFooter(message.author.tag, message.author.avatarURL)
-
-		message.channel.send(bcSure).then(msg => {
-			msg.react('✅').then(() => msg.react('❎'));
-			message.delete();
-
-
-			let yesEmoji = (reaction, user) => reaction.emoji.name === '✅'  && user.id === message.author.id;
-			let noEmoji = (reaction, user) => reaction.emoji.name === '❎' && user.id === message.author.id;
-
-			let sendBC = msg.createReactionCollector(yesEmoji);
-			let dontSendBC = msg.createReactionCollector(noEmoji);
-
-			sendBC.on('collect', r => {
-				message.guild.members.forEach(member => {
-					member.send(args1.replace(`[user]`, member)).catch();
-					if(message.attachments.first()){
-						member.sendFile(message.attachments.first().url).catch();
-					}
-				})
-				message.channel.send(`:timer: **يتم الان الارسال الى** \`\`${message.guild.memberCount}\`\` **عضو**`).then(msg => msg.delete(5000));
-				msg.delete();
-			});
-			dontSendBC.on('collect', r => {
-				msg.delete();
-				message.reply(':white_check_mark: **تم الغاء رسالتك**').then(msg => msg.delete(5000));
-			});
-		})
-	};
-
+    if(!message.channel.guild) return;
+     var success = new Discord.RichEmbed()
+     .setDescription(`تم أرسال رسالتك بنجاح.`)
+     .setColor('GREEN')
+ if(message.content.startsWith(prefix + 'bc')) {
+ if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
+ if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
+ let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+ let BcList = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setDescription(`**▶ 📝 لأرسال رسالة امبد قم بالضغط على \n ▶ ✏ لأرسال رسالة عادية قم بالضغط على \n ★ ${user} \n ★ ${server} \n ★ ${seender}**`)
+ if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(BcList).then(msg => {
+ msg.react('📝')
+ .then(() => msg.react('✏'))
+ .then(() =>msg.react('📝'))
+ 
+ let EmbedBcFilter = (reaction, user) => reaction.emoji.name === '📝' && user.id === message.author.id;
+ let NormalBcFilter = (reaction, user) => reaction.emoji.name === '✏' && user.id === message.author.id;
+ 
+ let EmbedBc = msg.createReactionCollector(EmbedBcFilter, { time: 60000 });
+ let NormalBc = msg.createReactionCollector(NormalBcFilter, { time: 60000 });
+ 
+ 
+ EmbedBc.on("collect", r => {
+ 
+ message.channel.send(success);
+ message.guild.members.forEach(m => {
+ let EmbedRep = args.replace('{server}' ,message.guild.name).replace('{user}', m).replace('{sender}', `${message.author}`)
+ var bc = new
+ Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setDescription(EmbedRep)
+ 
+ m.send({ embed: bc })
+ msg.delete();
+ })
+ })
+ NormalBc.on("collect", r => {
+   message.channel.send(success);
+ message.guild.members.forEach(m => {
+    let NormalRep = args.replace('{server}' ,message.guild.name).replace('{user}', m).replace('{sender}', `${message.author}`)
+ m.send(NormalRep);
+ msg.delete();
+ })
+ })
+ })
+ }
+ });
 
 client.login(process.env.BOT_TOKEN);
