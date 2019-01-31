9257311bdc77baf724e486ed464c7a3026858d4a
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-02-01 02:04:53 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,8 @@ client.on('ready', () => {
 	  console.log(`╔[ Channels [ " ${client.channels.size} " ]╗`);
 	  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
       console.log('')
-      client.user.setGame('-obc | BroadCast',`https://www.twitch.tv/BroadCast`);
+      client.user.setGame('-bc | BroadCast',`https://www.twitch.tv/BroadCast`);
+    });
 
 client.on("message", message => {
 
