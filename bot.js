800fda552b015b42e459ad52155274432852ce7c
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-02-01 02:09:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,5 +18,38 @@ client.on('ready', () => {
       console.log('')
       client.user.setGame('-bc | BroadCast',`https://www.twitch.tv/BroadCast`);
     });
-	
+
+client.on("message", message => {
+    if (message.content.startsWith("!obc")) {
+                 if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'all').forEach(m => {
+  m.send(`${argresult}\n ${m}`);
+  })
+  message.channel.send(`\`${message.guild.members.filter( m => m.presence.status !== 'all').size}\`:mailbox:  عدد المستلمين `);
+  message.delete();
+  };
+  });
+
+
+//bc online
+
+
+  var prefix = "!";
+
+  client.on("message", message => {
+  
+              if (message.content.startsWith(prefix + "bc")) {
+                           if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+    message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+   m.send(`${argresult}\n ${m}`);
+  })
+   message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` :mailbox:  عدد المستلمين `); 
+   message.delete(); 
+  };     
+  });
+
 client.login(process.env.BOT_TOKEN);
