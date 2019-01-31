8e85e0d6bac61a061c07066cc09f65fcb6a4ead2
Rezfix <44650563+Rezfix@users.noreply.github.com>
2019-02-01 02:06:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,31 +18,5 @@ client.on('ready', () => {
       console.log('')
       client.user.setGame('-bc | BroadCast',`https://www.twitch.tv/BroadCast`);
     });
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-
-
-client.on('message', message => {
-    if(!message.channel.guild) return;
-let args = message.content.split(' ').slice(1).join(' ');
-if (message.content.startsWith('-bcall')){
-if(!message.author.id === 'اي دي صاحب البوت') return;
-message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
-client.users.forEach(m =>{
-m.sendMessage(args)
-})
-}
-});
 	
 client.login(process.env.BOT_TOKEN);
